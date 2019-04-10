45bca17a52673ee0a8bbeb2b743b25ccf63b9188
Robert Knight <robertknight@gmail.com>
2019-04-10 23:19:51 +0100

    Implement new menu component
    Implement a new dropdown menu component that will replace all drop-down
    menus in the sidebar and is also intended to be used in the LMS activity
    views in future.
    
    The menu consists of several components:
    
     - `Menu` is the dropdown menu itself, with a toggle button and menu
       content
     - `MenuSection` is a group of menu items with an optional heading
     - `MenuItem` is a menu item with a label, optional icon and optional
       submenu with additional actions. The submenu is going to be used for
       auxilliary links and actions related to groups in the groups menu

---
+++ src/sidebar/components/menu.js
@@ -0,0 +1,149 @@
+'use strict';
+
+const { Fragment, createElement } = require('preact');
+const { useCallback, useEffect, useRef, useState } = require('preact/hooks');
+const propTypes = require('prop-types');
+
+const { listen } = require('../util/dom');
+
+const SvgIcon = require('./svg-icon');
+
+let ignoreNextClick = false;
+
+/**
+ * A drop-down menu.
+ */
+function Menu({
+  align = 'left',
+  children,
+  defaultOpen = false,
+  label,
+  menuIndicator = true,
+  title,
+}) {
+  const [isOpen, setOpen] = useState(defaultOpen);
+
+  // Toggle menu when user presses toggle button. The menu is shown on mouse
+  // press for a more responsive/native feel but also handles a click event for
+  // activation via other input methods.
+  const toggleMenu = event => {
+    // If the menu was opened on press, don't close it again on the subsequent
+    // mouse up.
+    if (event.type === 'mousedown') {
+      ignoreNextClick = true;
+    } else if (event.type === 'click' && ignoreNextClick) {
+      ignoreNextClick = false;
+      event.stopPropagation();
+      event.preventDefault();
+      return;
+    }
+    setOpen(!isOpen);
+  };
+  const closeMenu = useCallback(() => setOpen(false), [setOpen]);
+
+  // Close menu when user clicks outside or presses Esc.
+  const menuRef = useRef();
+  useEffect(() => {
+    if (!isOpen) {
+      return () => {};
+    }
+
+    const unlisten = listen(
+      document.body,
+      ['keypress', 'click', 'mousedown'],
+      event => {
+        if (event.type === 'keypress' && event.key !== 'Escape') {
+          return;
+        }
+        if (event.type === 'click' && ignoreNextClick) {
+          ignoreNextClick = false;
+          return;
+        }
+        if (
+          event.type === 'mousedown' &&
+          menuRef.current &&
+          menuRef.current.contains(event.target)
+        ) {
+          // Close the menu as soon as the user presses the mouse outside the
+          // menu, but only on clicks inside the menu.
+          return;
+        }
+        closeMenu();
+      }
+    );
+
+    return unlisten;
+  }, [closeMenu, isOpen]);
+
+  const menuStyle = {
+    [align]: 0,
+  };
+
+  return (
+    <div className="menu" ref={menuRef}>
+      <button
+        aria-expanded={isOpen ? 'true' : 'false'}
+        aria-haspopup={true}
+        className="menu__toggle"
+        onMouseDown={toggleMenu}
+        onClick={toggleMenu}
+        title={title}
+      >
+        {label}
+        {menuIndicator && (
+          <span className="menu__toggle-arrow">
+            <SvgIcon name="expand-menu" />
+          </span>
+        )}
+      </button>
+      {isOpen && (
+        <Fragment>
+          <div className="menu__arrow" />
+          <div className="menu__content" role="menu" style={menuStyle}>
+            {children}
+          </div>
+        </Fragment>
+      )}
+    </div>
+  );
+}
+
+Menu.propTypes = {
+  /**
+   * Whether the menu content is aligned with the left (default) or right edges
+   * of the toggle element.
+   */
+  align: propTypes.oneOf(['left', 'right']),
+
+  /**
+   * Label element for the toggle button that hides and shows the menu.
+   */
+  label: propTypes.object.isRequired,
+
+  /**
+   * Menu items and sections to display in the content area of the menu.
+   *
+   * These are typically `MenuSection` and `MenuItem` components, but other
+   * custom content is also allowed.
+   */
+  children: propTypes.any,
+
+  /**
+   * Whether the menu is open or closed when initially rendered.
+   */
+  defaultOpen: propTypes.bool,
+
+  /**
+   * A title for the menu. This is important for accessibility if the menu's
+   * toggle button has only an icon as a label.
+   */
+  title: propTypes.string.isRequired,
+
+  /**
+   * Whether to display an indicator next to the label that there is a
+   * dropdown menu.
+   */
+  menuIndicator: propTypes.bool,
+};
+
+module.exports = Menu;
